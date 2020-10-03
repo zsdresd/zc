b3b0fc8d0f2fbfb08f4eb86fb14e28466670f553
zsdresd <60921377+zsdresd@users.noreply.github.com>
2020-10-03 17:52:55 +0800

    Update zipupfan.sh

--- zipupfan.sh
+++ zipupfan.sh
@@ -1,7 +1,6 @@
 function up() 
 { 
 fclone move /root/$1 gc:{$3}/$1-$2 --ignore-existing --stats=1s --stats-one-line --checkers=512 --transfers=512 --drive-pacer-min-sleep=1ms --check-first
-fclone move /root/$1-$2.zip gc:{$3} --ignore-existing --stats=1s --stats-one-line --checkers=64 --transfers=64 --drive-pacer-min-sleep=1ms --check-first
 }
 arr=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
 num=1480001
@@ -10,9 +9,6 @@ do
 for value2 in ${arr[@]}
 do
  ((bnum=$num+9999))
- if [ ! -f "$num-$bnum.zip" ];then
-  echo sws
- else
  if  [ $num -gt 0 ] && [ $num -lt 300000 ] ; then
   up $num $bnum 0ADR52XSOV6bDUk9PVA
   up $num $bnum 0ADR52XSOV6bDUk9PVA
@@ -28,7 +24,6 @@ do
  elif [ $num -gt 120000 ] && [ $num -lt 1500000 ] ; then
   up $num $bnum 0ALjr3FAoWexDUk9PVA
   up $num $bnum 0ALjr3FAoWexDUk9PVA
- fi
  fi
  ((num-=10000))
 done
